903b7775d2044d824b9c2a992f48cc4c0c29b305
Abdirazakjama <jaamacciidh@gmail.com>
2023-09-17 01:29:02 -0700

    update

--- backend/index.js
+++ backend/index.js
@@ -35,6 +35,19 @@ app.get('/',(req,res)=>{
 app.use('/books',router);
 
 
+if (process.env.NODE_ENV === 'production') {
+
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+} else {
+    app.get('/books', (req, res) => {
+        res.send('API is running');
+    });
+}
 
 
 
